4d4d373db7242a0f110f954c2acb929e90eca2bc
Антонина Максимова <tonkastar@mail.ru>
2020-11-29 21:53:42 +0300

    no message

--- src/App.js
+++ src/App.js
@@ -1,31 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
-import { count, length } from './number';
+import React from 'react';
 
-const numn = 23;
+const elem = (<div>Elementum</div>);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          count * length = { count * length }
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export { elem };
--- src/number.js
+++
@@ -1,4 +0,0 @@
-const count = 44;
-const length = 34;
-
-export { count, length };
