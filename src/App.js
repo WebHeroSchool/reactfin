ca8c0844b4d4142d07a12253545fbf8bd55f983d
Антонина Максимова <tonkastar@mail.ru>
2020-11-30 11:41:18 +0300

    no message

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
-const elem = (<div>Elementum</div>);
+const ItemList = () => (
+  <ol>
+  <li>First task</li>
+  <li>Second task</li>
+  <li>Third task</li>
+  </ol>
+)
 
-export { elem };
+const App = () => (<div>
+  <h1>Elementum</h1>
+  <ItemList />
+  </div>);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { elem } from './App';
+import App from './App';
 
-ReactDOM.render(elem,
+ReactDOM.render(<App />,
   document.getElementById('root')
 );
