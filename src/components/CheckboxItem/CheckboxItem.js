e1af4b272b0458fa076189658153f33fa3e4eda4
Антонина Максимова <tonkastar@mail.ru>
2020-12-05 15:22:59 +0300

    no message

--- src/components/CheckboxItem/CheckboxItem.js
+++ src/components/CheckboxItem/CheckboxItem.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Checkbox from '@material-ui/core/Checkbox';
+// import styles from './CheckboxItem.module.css';
 
-const CheckboxItem = () => (<div>
+const CheckboxItem = () => (
 	<Checkbox
         defaultChecked
         size="small"
         inputProps={{ 'aria-label': 'checkbox with small size' }}
-    />
-</div>);
+    />);
 
 export default CheckboxItem;
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Item from '../Item/Item';
 import CheckboxItem from '../CheckboxItem/CheckboxItem';
+import styles from './ItemList.module.css';
 
 const ItemList = ({items}) => (
-  <ol>
+  <li className={styles.list}>
   {items.map(item =><li key = {item.value}>{<CheckboxItem />}<Item value = { item.value } isDone = { item.isDone }/></li>)}
-  </ol>
+  </li>
 )
 
 export default ItemList;
