59c60d9361b581e704d3561ec7333af8130a0057
Антонина Максимова <tonkastar@mail.ru>
2020-12-06 18:26:08 +0300

    no message

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,8 +6,9 @@ import TextFieldItem from '../TextField/TextField';
 import DeleteAllComplete from '../DeleteAllComplete/DeleteAllComplete';
 import TaskFilter from '../TaskFilter/TaskFilter';
 
-const App = () => {
-	const items = [
+class App extends React.Component {
+	render() {
+		const items = [
 		{	
 			value: 'Выполнить домашнюю работу',
 			isDone: true
@@ -30,7 +31,34 @@ const App = () => {
 		<ItemList items = { items }/>
 		<DeleteAllComplete />
 		<Footer count = {3} />
-  	</div>);
-  }
+  	</div>)};
+};
+
+// const App = () => {
+// 	const items = [
+// 		{	
+// 			value: 'Выполнить домашнюю работу',
+// 			isDone: true
+// 		},
+// 		{
+// 			value: 'Убраться',
+// 			isDone: true
+// 		},
+// 		{
+// 			value: 'Прогуляться по набережной',
+// 			isDone: false
+// 		}	
+// 	];
+
+// 	return (
+// 	<div className={styles.wrap}>
+// 		<h1 className={styles.title}>Задачи на сегодня</h1>
+// 		<TaskFilter />
+// 		<TextFieldItem />
+// 		<ItemList items = { items }/>
+// 		<DeleteAllComplete />
+// 		<Footer count = {3} />
+//   	</div>);
+//   }
 
 export default App;
