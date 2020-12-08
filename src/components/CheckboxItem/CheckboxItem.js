8ba72a72108c56fd2f193269d31a1508622de30c
Антонина Максимова <tonkastar@mail.ru>
2020-12-08 16:10:41 +0300

    no message

--- src/components/CheckboxItem/CheckboxItem.js
+++ src/components/CheckboxItem/CheckboxItem.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Checkbox from '@material-ui/core/Checkbox';
 
-const CheckboxItem = () => (
+const CheckboxItem = (isDone, OnClickDone) => (
 	<Checkbox
-        defaultChecked
+        checked={isDone}
+		onClick ={() => OnClickDone(isDone)}
         size="small"
         inputProps={{ 'aria-label': 'checkbox with small size' }}
     />);
